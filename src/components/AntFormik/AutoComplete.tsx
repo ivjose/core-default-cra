592bd97cdc440a77b3caadbbb77de778ff8eefee
ivjose <ivjosesantos@gmail.com>
2019-06-30 10:21:39 +0800

    [FEATURE] Update all forms add custom FormItem

--- src/components/AntFormik/AutoComplete.tsx
+++ src/components/AntFormik/AutoComplete.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
-import { AutoComplete as AntAutoComplete, Form } from 'antd';
+import { AutoComplete as AntAutoComplete } from 'antd';
 import { Field, FieldProps } from 'formik';
 import { FormikFieldProps } from './FieldProps';
 import { AutoCompleteProps as AntAutoCompleteProps } from 'antd/lib/auto-complete';
-
-const FormItem = Form.Item;
+import { FormItem } from './FormItem';
 
 export const AutoComplete: React.FC<FormikFieldProps & AntAutoCompleteProps> = ({
   name,
@@ -13,13 +12,8 @@ export const AutoComplete: React.FC<FormikFieldProps & AntAutoCompleteProps> = (
   ...restProps
 }) => (
   <Field name={name}>
-    {({ field: { value }, form: { touched, errors, setFieldValue, setFieldTouched } }: FieldProps) => (
-      <FormItem
-        required={required}
-        label={label}
-        validateStatus={touched[name] && errors[name] ? 'error' : ''}
-        help={touched[name] && errors[name]}
-      >
+    {({ field: { value }, form: { setFieldValue, setFieldTouched } }: FieldProps) => (
+      <FormItem name={name} required={required} label={label}>
         <AntAutoComplete
           value={value}
           onChange={val => setFieldValue(name, val.valueOf())}
